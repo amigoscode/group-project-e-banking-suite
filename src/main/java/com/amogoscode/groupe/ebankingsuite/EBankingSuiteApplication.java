77f0ff89661e5470170874f72a7108219e575e80
larwal <larwalolakunle@gmail.com>
2023-02-20 10:21:05 +0100

    project setup with dependencies

---
+++ src/main/java/com/amogoscode/groupe/ebankingsuite/EBankingSuiteApplication.java
@@ -0,0 +1,13 @@
+package com.amogoscode.groupe.ebankingsuite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBankingSuiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EBankingSuiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amogoscode/groupe/ebankingsuite/EBankingSuiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amogoscode.groupe.ebankingsuite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBankingSuiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
